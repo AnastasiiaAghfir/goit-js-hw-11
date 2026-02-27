8190fb3080f1942fa75d629b02dc6b2ee63fa20f
Anastasiia_Aghfir <anastasiia.aghfir@gmail.com>
2026-02-27 23:39:19 +0100

    hw-11

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://pixabay.com/api/";
+const API_KEY = "54828385-3257320bcaeec570fd5a64756";
+
+export function getImagesByQuery(query) {
+  return axios.get(BASE_URL, {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: "photo",
+      orientation: "horizontal",
+      safesearch: true,
+    },
+  });
+}
